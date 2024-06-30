c0c5fffe5ef782f7c71a352a560a8fb8af2fb4cf
Franz <32547553+ahhfranz@users.noreply.github.com>
2024-06-30 12:52:17 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
             return false;
         }
 
-        const phone = '5491162089720';      
+        const phone = '5491159098742';      
 let msg = ' 🍀 ¡Hola! Quiero mi usuario y el bonus de bienvenida:\n';
         msg += '*Nombre:* ' + user.value + "\n";
        
